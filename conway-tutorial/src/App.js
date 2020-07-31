4f52a302c356572ac096b14c59b6d83bbf4f9834
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-31 16:37:34 -0500

    Added some more detail on Turing machines.

--- conway-tutorial/src/App.js
+++ conway-tutorial/src/App.js
@@ -172,6 +172,49 @@ const App = () => {
                     </ul>
                     For each iteration (or generation as I'm calling them), each
                     cell will be evaluated according to these rules and will
+                    either die or be reborn according to the rules provided. The
+                    Game of Life is an example of a Turing Complete automata.
+                    This means that it can function as a Turing Machine. A
+                    Turing Machine is a theoretical machine posited by British
+                    mathematician Alan Turing.
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                    Turing Machine
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="2">
+                  <Card.Body>
+                    <h4>A Turing Machine: </h4>
+                    <ul>
+                      <li>
+                        Imagines an arbitrarily long magnetic tape divided into
+                        squares with values printed onto each square (1, 0, or
+                        blank).
+                      </li>
+                      <li>
+                        Also imagines a reader which moves along the length of
+                        the tape and reads the values at each index.
+                      </li>
+                      <li>
+                        When the reader finds a value, it has coded instructions
+                        for what to do with that value (if value x, do y, then
+                        enter state z).
+                      </li>
+                      <li>
+                        Finally, there is a finishing state whereby the Turing
+                        Machine completes its program and ends computation.
+                      </li>
+                      <li>
+                        The assumption is that a detailed program given to a
+                        Turing Machine can solve a problem that it is provided.
+                      </li>
+                    </ul>
+                    For each iteration (or generation as I'm calling them), each
+                    cell will be evaluated according to these rules and will
                     either die or be reborn according to the rules provided.
                   </Card.Body>
                 </Accordion.Collapse>
